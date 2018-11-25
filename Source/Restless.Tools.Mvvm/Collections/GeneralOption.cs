6ae74ebcfb0970f15332419897c3a7ba8ab10dc2
Victor D. Sandiego <codepuppy@balticsoap.com>
2018-11-25 15:57:34 -0600

    Re-organize mvvm folders. Add collection related classes

---
+++ Source/Restless.Tools.Mvvm/Collections/GeneralOption.cs
@@ -0,0 +1,44 @@
+﻿namespace Restless.Tools.Mvvm.Collections
+{
+    /// <summary>
+    /// Represents a general purpose helper class that can be used when binding.
+    /// </summary>
+    public class GeneralOption : SelectableItem
+    {
+        #region Private
+        #endregion
+
+        /************************************************************************/
+
+        #region Public properties
+        /// <summary>
+        /// Gets or sets a string value associated with this option.
+        /// </summary>
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an integer value associated with this option.
+        /// </summary>
+        public long IntValue
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        /************************************************************************/
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneralOption"/> class.
+        /// </summary>
+        public GeneralOption()
+        {
+        }
+        #endregion
+    }
+}
---
+++ Source/Restless.Tools.Mvvm/Collections/SelectableItemList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Restless.Tools.Mvvm.Collections
+{
+    /// <summary>
+    /// Represents a list of <see cref="SelectableItem"/> objects.
+    /// </summary>
+    public class SelectableItemList<T> : ObservableCollection<T> where T : SelectableItem
+    {
+        #region Private
+        private bool isSelectChangedActive;
+        #endregion
+
+        /************************************************************************/
+
+        #region Public events
+        /// <summary>
+        /// Occurs when an item of the collection has changed its <see cref="SelectableItem.IsSelected"/> property.
+        /// You must use the <see cref="AddObservable(T)"/> method for each item added to the collection 
+        /// to enable this event.
+        /// </summary>
+        public event EventHandler IsSelectedChanged;
+        #endregion
+
+        /************************************************************************/
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectableItemList{T}"/> class.
+        /// </summary>
+        public SelectableItemList()
+        {
+            isSelectChangedActive = true;
+        }
+        #endregion
+
+        /************************************************************************/
+
+        #region Public methods
+        /// <summary>
+        /// Adds an item to the collection and hooks up its <see cref="SelectableItem.IsSelectedChanged"/> event.
+        /// </summary>
+        /// <param name="item">The item to add</param>
+        public void AddObservable(T item)
+        {
+            Add(item);
+            if (item != null)
+            {
+                item.Index = Count - 1;
+                item.IsSelectedChanged += ItemIsSelectedChanged;
+            }
+        }
+
+        /// <summary>
+        /// Selects all items in the list.
+        /// </summary>
+        public void SelectAll()
+        {
+            foreach (T item in this)
+            {
+                item.IsSelected = true;
+            }
+        }
+
+        /// <summary>
+        /// Deselects all items in the list.
+        /// </summary>
+        public void DeselectAll()
+        {
+            foreach (T item in this)
+            {
+                item.IsSelected = false;
+            }
+        }
+
+        /// <summary>
+        /// Suspends firing of the <see cref="IsSelectedChanged"/> event.
+        /// </summary>
+        public void SuspendIsSelectChanged()
+        {
+            isSelectChangedActive = false;
+        }
+
+        /// <summary>
+        /// Resumes firing of the <see cref="IsSelectedChanged"/> event.
+        /// </summary>
+        public void ResumeIsSelectChanged()
+        {
+            isSelectChangedActive = true;
+        }
+        #endregion
+
+        /************************************************************************/
+
+        #region Private events
+        private void ItemIsSelectedChanged(object sender, EventArgs e)
+        {
+            if (isSelectChangedActive)
+            {
+                IsSelectedChanged?.Invoke(sender, e);
+            }
+        }
+        #endregion
+    }
+}
