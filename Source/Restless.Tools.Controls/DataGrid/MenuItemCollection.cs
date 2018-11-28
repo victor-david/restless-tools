47eede01d85dbb8cad945a5d7e9e4bbe165cd193
Victor D. Sandiego <codepuppy@balticsoap.com>
2018-11-27 21:29:12 -0600

    Modify MenuItemCollection to simplify add items. Too confusing with all the parms. Added MenuItem extensions to enable clarity of intention when adding an item.

--- Source/Restless.Tools.Controls/DataGrid/MenuItemCollection.cs
+++ Source/Restless.Tools.Controls/DataGrid/MenuItemCollection.cs
@@ -9,6 +9,9 @@ namespace Restless.Tools.Controls
     /// <summary>
     /// Represents a bindable collection of menu items with convienence methods for adding items.
     /// </summary>
+    /// <remarks>
+    /// See also <see cref="MenuItemExtensions"/>
+    /// </remarks>
     public class MenuItemCollection : ObservableCollection<Control>
     {
         /// <summary>
@@ -16,25 +19,18 @@ namespace Restless.Tools.Controls
         /// </summary>
         /// <param name="header">The item header, that which is displayed in the UI</param>
         /// <param name="command">The command associated with this item.</param>
-        /// <param name="commandParm">A parameter for <paramref name="command"/> if needed, or null if none.</param>
-        /// <param name="imageResource">The name of the image resource to use on this item, or null if none.</param>
-        /// <param name="tag">An arbitrary object to attach to the menu item.</param>
-        public void AddItem(string header, ICommand command, object commandParm = null, string imageResource = null, object tag = null)
+        /// <returns>The newly added item.</returns>
+        public MenuItem AddItem(string header, ICommand command)
         {
             var item = new MenuItem
             {
                 Header = header,
                 Command = command,
-                CommandParameter = commandParm,
-                Tag = tag,
                 HorizontalContentAlignment = HorizontalAlignment.Left,
                 VerticalContentAlignment = VerticalAlignment.Center
             };
-            if (!string.IsNullOrEmpty(imageResource))
-            {
-                item.Icon = Application.Current.TryFindResource(imageResource);
-            }
             Add(item);
+            return item;
         }
 
         /// <summary>
---
+++ Source/Restless.Tools.Controls/DataGrid/MenuItemExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Restless.Tools.Controls
+{
+    /// <summary>
+    /// Provides convienence extension methods for <see cref="MenuItem"/>.
+    /// </summary>
+    public static class MenuItemExtensions
+    {
+        /// <summary>
+        /// Adds an image resource to the menu item.
+        /// </summary>
+        /// <param name="item">The item</param>
+        /// <param name="imageResource">The string name of the image resource</param>
+        /// <returns>The item</returns>
+        public static MenuItem AddImageResource(this MenuItem item, string imageResource)
+        {
+            if (!string.IsNullOrEmpty(imageResource))
+            {
+                item.Icon = Application.Current.TryFindResource(imageResource);
+            }
+            return item;
+        }
+
+        /// <summary>
+        /// Adds a tag to the menu item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        public static MenuItem AddCommandParm(this MenuItem item, object parm)
+        {
+            item.CommandParameter = parm;
+            return item;
+        }
+
+        /// <summary>
+        /// Adds a tag to the menu item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public static MenuItem AddTag(this MenuItem item, object tag)
+        {
+            item.Tag = tag;
+            return item;
+        }
+    }
+}
