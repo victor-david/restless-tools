c2c2b9f68d5d424ba383e5eee0d6376d918f6ffb
Victor D. Sandiego <codepuppy@balticsoap.com>
2020-04-18 12:21:25 -0500

    Update Open Xml package
    Update OpenXml package and change assembly version to 1.3.3.0

--- Source/Restless.Tools.OpenXml/Properties/AssemblyInfo.cs
+++ Source/Restless.Tools.OpenXml/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5a4ce6b3-aeef-41dd-a3fb-422380879f7a")]
 
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
